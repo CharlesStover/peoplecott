3ba64f87fc0357395d5cc4eaf27cd06a282ef146
Charles Stover (via Travis CI) <github@charlesstover.com>
2020-02-24 02:46:49 +0000

    Deploy CharlesStover/peoplecott to github.com/CharlesStover/peoplecott.git:gh-pages

---
+++ precache-manifest.c8b753c2896c99297084f1ec390f89c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146b3211a0bd0b97a2cfbc346135bd21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e19fe9a4018b84c88912",
+    "url": "/static/css/main.cd49c8b0.chunk.css"
+  },
+  {
+    "revision": "3eed36b4dbc1660fba9e",
+    "url": "/static/js/2.50b4e2b2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.50b4e2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19fe9a4018b84c88912",
+    "url": "/static/js/main.ab797527.chunk.js"
+  },
+  {
+    "revision": "4900c34f762e88e47bd4",
+    "url": "/static/js/runtime-main.96fd9e1e.js"
+  }
+]);
