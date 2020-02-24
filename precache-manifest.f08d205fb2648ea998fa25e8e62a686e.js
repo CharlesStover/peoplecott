50b1780e25419b59f9202a6edaa658c26e512af3
Charles Stover (via Travis CI) <github@charlesstover.com>
2020-02-24 02:48:14 +0000

    Deploy CharlesStover/peoplecott to github.com/CharlesStover/peoplecott.git:gh-pages

---
+++ precache-manifest.f08d205fb2648ea998fa25e8e62a686e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b573bb4a2de81b9debf262e283fc6a70",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64874412516068e2a60d",
+    "url": "./static/css/main.cd49c8b0.chunk.css"
+  },
+  {
+    "revision": "cecb0bf89f17cf97dfbd",
+    "url": "./static/js/2.5008b41e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.5008b41e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64874412516068e2a60d",
+    "url": "./static/js/main.2fb55da9.chunk.js"
+  },
+  {
+    "revision": "4e6c44e65102a9b9dc9d",
+    "url": "./static/js/runtime-main.3d8111af.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8b753c2896c99297084f1ec390f89c1.js"
+  "./precache-manifest.f08d205fb2648ea998fa25e8e62a686e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
